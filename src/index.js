d272d844990a0b5602717fe99eff30d83ff4cb0d
liemdang260 <69330905+liemdang260@users.noreply.github.com>
2021-11-03 09:51:24 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const mainRouter = require('./routers/main-router')
 const dotenv = require('dotenv')
 const { connectDB2 } = require('./connectDB/db')
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use('/', mainRouter)
 
 const startServer = async () => {
--- src/routers/main-router.js
+++ src/routers/main-router.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const authenticationRouter = require("../modules/authentication/authentication.router")
 const userRouter = require('../modules/user/user.router')
+router.use((req,res,next)=>{
+    console.log(req.method,req.url)
+    return next();
+})
 router.use('/users',userRouter)
 router.get('/',(req,res)=>{
     res.send("APP RUNNING AT PORT 9999")
