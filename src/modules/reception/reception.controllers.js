588362c38db3184022799a20bcf03c2b43382415
Lspau <71479445+Lspau@users.noreply.github.com>
2021-11-10 19:04:59 +0700

    create

--- src/modules/reception/reception.controllers.js
+++ src/modules/reception/reception.controllers.js
@@ -8,21 +8,38 @@ exports.createAppointment = async (req, res) => {
             })
         }
         let id = req.body.PATIENT_ID
-        let appointment = APPOINTMENT.findOne({
-            where: { PATIENT_ID: id }
+        let patient = PATIENT.findOne({
+            where:{PATIENT_ID: id}
         })
-        if (!appointment) {
-            appointment = new reception({
+        if (!patient){
+                patient = new patient({
+                 PATIENT_ID: req.body.PATIENT_ID,
+                 PATIENT_NAME: req.boy.PATIENT_NAME,
+                 IDENTITY_NUMBER: req.body.IDENTITY_NUMBER,
+                 PHONE: req.body.PHONE,
+                 GENDER: req.body.GENDER,
+                 DATE_OF_BIRTH: req.body.DATE_OF_BIRTH,
+                 ADDRESS: req.body.ADDRESS,
+                })
+                patient.save()
+                appointment = new appointment({
+                    APPOINTMENT_ID: req.patient.APPOINTMENT_ID,
+                    PATIENT_ID: req.patient.PATIENT_ID,
+                    DOCTOR_ID: req.patient.DOCTOR_ID,
+                    TIMES: req.patient.TIMES,
+                })
+                appointment.save()
+            return res.status(200).send('Đã thêm khách hàng và lịch hẹn thành công!')
+        }
+        else {
+            appointment = new appointment({
                 APPOINTMENT_ID: req.body.APPOINTMENT_ID,
                 PATIENT_ID: req.body.PATIENT_ID,
                 DOCTOR_ID: req.body.DOCTOR_ID,
                 TIMES: req.body.TIMES,
             })
             appointment.save()
-        }
-        else {
-            return res.send('ID đã có rồi!')
-
+            return res.status(200).send('Đã thêm lịch hẹn thành công!')
         }
     } catch (e) {
         return res.status(500).send('Lỗi sever!')
